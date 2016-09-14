f33e622ecc7315e784de51c7cc4619a543bb7bb5
McLoy <tkachenko.vlad@hotmail.com>
2016-09-15 01:16:04 +0300

    update

--- src/com/ua/vtkachenko/EasyTask3/EasyTask3.java
+++ src/com/ua/vtkachenko/EasyTask3/EasyTask3.java
@@ -6,9 +6,9 @@ public class EasyTask3 {
     public static void main(String[] args) {
 
         HashSet<Cat> cats = createCats(new Cat("Porter"), new Cat("Garfild"), new Cat("Zorro"));
-//        for (Cat f : cats)
-//            System.out.println(f.name);
-//        System.out.println("Deleting one of cats from Set");
+        for (Cat f : cats)
+            System.out.println(f.name);
+        System.out.println("Deleting one of cats from Set");
 
         Iterator<Cat> iter = cats.iterator();
         while(iter.hasNext()){
