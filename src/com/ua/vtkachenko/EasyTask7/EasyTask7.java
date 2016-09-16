c2bd5645b77bb8d88186a073d77c2622816f770f
McLoy <tkachenko.vlad@hotmail.com>
2016-09-17 02:33:33 +0300

    update

--- src/com/ua/vtkachenko/EasyTask7/EasyTask7.java
+++ src/com/ua/vtkachenko/EasyTask7/EasyTask7.java
@@ -4,7 +4,7 @@ package com.ua.vtkachenko.EasyTask7;
 public class EasyTask7 {
     public static void main(String[] args) {
         countCode("aaacodebbb", "code");// → 1
-        countCode("fgsfgdfsahfsdfsdghwowowphjghdfgfghjolbrawowebbb", "wow");
+        countCode("fgsfgdfsahfsdfsdghwowowphjghdfjolbrawowebbb", "wow");
     }
     public static void countCode(String in, String word){
 
