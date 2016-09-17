b48d1658df5707f467e34d4fdde1c14006a95f83
McLoy <tkachenko.vlad@hotmail.com>
2016-09-17 14:36:35 +0300

    update

--- src/com/ua/vtkachenko/EasyTask8/EasyTask8.java
+++ src/com/ua/vtkachenko/EasyTask8/EasyTask8.java
@@ -1,9 +1,8 @@
 package com.ua.vtkachenko.EasyTask8;
 
 import java.io.*;
-import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
-import java.util.StringTokenizer;
 
 //сделай класс фильтр матюков например swearingFilter.filter("Я ненавижу этот ебаный мир") -> "Я ненавижу этот @#%& мир" сделай так чтоб список матерных слов можно было пополнять
 public class EasyTask8 {
@@ -16,105 +15,63 @@ public class EasyTask8 {
         String d = "Да мне похуй на все";
         String e = "Охуели совсем";
 
-        System.out.println("Словарь: " + swearingFilter.fileReading().toString());
+        swearingFilter.filter(a);
+        swearingFilter.filter(b);
+        swearingFilter.filter(c);
+        swearingFilter.filter(d);
+        swearingFilter.filter(e);
 
-//        swearingFilter.filter(a);
-//        swearingFilter.filter(b);
-//        swearingFilter.filter(c);
-//        swearingFilter.filter(d);
-//        swearingFilter.filter(e);
-
-        //System.out.println(System.getProperty("user.dir"));
-        //System.out.println(System.getProperty("user.home"));
-
-
-        //swearingFilter.writeFile("сука", voc);
-
-//        String voc = "SwearingWords.txt";
-//        String dir = System.getProperty("user.dir");
-//        String link = dir + System.getProperty("file.separator")+ voc;
-
-//        File newFile = new File(link);
-//        if (newFile.createNewFile()) {
-//            System.out.println("Новый файл создан");
-//            System.out.println("Заполните список плохих слов(через пробел)");
-//            Scanner sc = new Scanner(System.in);
-//            String badWords = sc.nextLine();
-//
-//            //Запись в файл
-//            try(FileWriter writer = new FileWriter(link, false))
-//            {
-//                // запись всей строки
-//                writer.write(badWords);
-//                // запись по символам
-////                writer.append('\n');
-////                writer.append('E');
-//
-//                writer.flush();
-//            }
-//            catch(IOException ex){
-//
-//                System.out.println(ex.getMessage());
-//            }
-//        } else {
-//            System.out.println("Файл уже существует");
-
-//            StringBuilder badBoxWords = new StringBuilder();
-//
-//            //Читаем файл
-//            try(FileReader reader = new FileReader(link))
-//            {
-//                // читаем посимвольно
-//                int cc;
-//                while((cc = reader.read())!=-1){
-//
-//                    //System.out.print((char)cc);
-//                    badBoxWords.append((char)cc);
-//                }
-//            }
-//            catch(IOException ex){
-//
-//                System.out.println(ex.getMessage());
-//            }
-
-//            System.out.println(badBoxWords.toString());
-//
-//            String rr = badBoxWords.toString().trim() + " ";
-
-//            int count = 0;
-//            String wr = "";
-//            for (int i = 0; i < rr.length(); i++) {
-//                if (rr.charAt(i) == ' ') {
-//                   wr = rr.substring(count, i);
-//                    //System.out.println(wr);
-//                    count = i + 1;
-//
-//                    //Делаем замену плохих слов
-//                    StringBuilder wrep = new StringBuilder();
-//                    wrep.append(a);
-//                    int inStart = wrep.indexOf(wr);
-//                    int inFin = wrep.indexOf(wr) + wr.length();
-//
-//                    wrep.replace(inStart, inFin, "**");
-//                    System.out.println(wrep.toString());
-//                    break;
-
-//                }
-//            }
-//        }
-//
-//
-//
     }
 }
 
 class swearingFilter {
 
-//    public static void filter(String enter) {
-//
-//
-//
-//    }
+    static void filter(String str) {
+
+        try {
+            StringBuilder bads = fileReading();
+
+        String rr = bads.toString().trim() + " ";
+
+            int count = 0;
+            //String wr = "";
+            for (int i = 0; i < rr.length(); i++) {
+                if (rr.charAt(i) == ' ') {
+                   String wr = rr.substring(count, i);
+                    //System.out.println(wr);
+                    count = i + 1;
+
+                    //Делаем замену плохих слов
+                    StringBuilder givenString = new StringBuilder();
+                    givenString.append(str);
+                    int inStart = givenString.indexOf(wr);
+                    if (inStart != -1) {
+                        int inFin = givenString.indexOf(wr) + wr.length();
+
+                        //Определим длину заменяемых символов
+                        int len = inFin - inStart;
+                        //System.out.println(len);
+
+                        String rep = "";
+                        for (int j = 0; j < len; j++) {
+                            Random r = new Random();
+                            String dict = "@#$%^&*"; //строка содержит все доступные символы
+                            char c = dict.charAt(r.nextInt(dict.length())); //в переменную с записывается случайный из них
+                            rep = rep + c;
+                        }
+                        //System.out.println(rep);
+
+                        givenString.replace(inStart, inFin, rep);
+                        System.out.println(givenString.toString());
+                    }
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 
     static StringBuilder fileReading() throws IOException {
 
@@ -162,6 +119,5 @@ class swearingFilter {
         }
         return badWordsString;
     }
-
 
 }
