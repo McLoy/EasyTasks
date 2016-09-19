1b730ff79eae71ff3c8c1e6dbf0280d83c09cb63
McLoy <tkachenko.vlad@hotmail.com>
2016-09-19 10:40:49 +0300

    update

--- src/com/ua/vtkachenko/EasyTask9/StringSample.java
+++ src/com/ua/vtkachenko/EasyTask9/StringSample.java
@@ -1,8 +1,11 @@
 package com.ua.vtkachenko.EasyTask9;
 
+import java.util.*;
+
 public class StringSample {
     public static void main(String[] args) {
         int d = 10;
         System.out.println(d + "7");
+
     }
 }
