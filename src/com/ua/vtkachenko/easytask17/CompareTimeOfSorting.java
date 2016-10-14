6087d49076a9a715bf9634189502eb8574e48883
McLoy <tkachenko.vlad@hotmail.com>
2016-10-14 17:33:51 +0300

    update

--- src/com/ua/vtkachenko/easytask17/CompareTimeOfSorting.java
+++ src/com/ua/vtkachenko/easytask17/CompareTimeOfSorting.java
@@ -9,6 +9,7 @@ public class CompareTimeOfSorting {
 
 
         int[] a = new int[3000];
+        int t = 0;
 
         Random r = new Random();
 
@@ -20,6 +21,7 @@ public class CompareTimeOfSorting {
 
         //int[] b = (int[])a.clone();
         int[] b = a.clone();
+        int[] c = a.clone();
         //System.arraycopy(a, 0, b, 0, a.length);
         //int[] c = a.clone();
 
@@ -29,7 +31,7 @@ public class CompareTimeOfSorting {
         for (int j = a.length - 1; j > 0; j--) {
             for (int i = 0; i < j; i++) {
                 if (a[i] > a[i + 1]) {
-                    int t = a[i + 1];
+                    t = a[i + 1];
                     a[i + 1] = a[i];
                     a[i] = t;
                 }
@@ -46,7 +48,7 @@ public class CompareTimeOfSorting {
         //Simple Sorting
         for (int i = 0; i < b.length - 1;) {
             if (b[i] > b[i + 1]){
-                int t = b[i + 1];
+                t = b[i + 1];
                 b[i + 1] = b[i];
                 b[i] = t;
                 i = 0;
@@ -56,6 +58,33 @@ public class CompareTimeOfSorting {
         }
         finish = System.currentTimeMillis();
         System.out.println(Arrays.toString(b));
+        msec = (int) (finish - start);
+        System.out.println("Time of sorting: " + msec / 1000 + " sec" + " or " + msec + "msec");
+
+        System.out.println("+++Shaker sorting+++");
+        System.out.println(Arrays.toString(c));
+        int z = 0;
+        start = System.currentTimeMillis();
+
+        //Shaker Sorting
+        for (int j = c.length - 1; j > 0; j--) {
+            for (int i = 0; i < j; i++) {
+                if (c[i] > c[i + 1]) {
+                    t = c[i + 1];
+                    c[i + 1] = c[i];
+                    c[i] = t;
+                }
+            }
+            for (int k = j - 1; k >= c.length - j; k--) {
+                if (c[k] < c[k - 1]) {
+                    t = c[k - 1];
+                    c[k - 1] = c[k];
+                    c[k] = t;
+                }
+            }
+        }
+        finish = System.currentTimeMillis();
+        System.out.println(Arrays.toString(c));
         msec = (int) (finish - start);
         System.out.println("Time of sorting: " + msec / 1000 + " sec" + " or " + msec + "msec");
     }
