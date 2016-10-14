3bde37f2d4ba14285e4a986f67db2d87a1f2b4ac
McLoy <tkachenko.vlad@hotmail.com>
2016-10-14 17:01:21 +0300

    Bubble sorting with time of sorting

--- src/com/ua/vtkachenko/easytask18/ShakerSortingArray.java
+++ src/com/ua/vtkachenko/easytask18/ShakerSortingArray.java
@@ -1,18 +1,18 @@
-package com.ua.vtkachenko.easytask16;
+package com.ua.vtkachenko.easytask18;
 
 import java.util.Arrays;
 import java.util.Random;
 
-public class BubbleSortOfArray {
+public class ShakerSortingArray {
 
     public static void main(String[] args) {
 
-        int[] a = new int[40000];
+        int[] a = new int[9];
 
         Random r = new Random();
 
         for (int i = 0; i < a.length; i++) {
-            a[i] = r.nextInt(1000000);
+            a[i] = r.nextInt(1000);
         }
 
         System.out.println(Arrays.toString(a));
@@ -20,7 +20,7 @@ public class BubbleSortOfArray {
         long start = System.currentTimeMillis();
 
 
-        //Bubble Sorting
+        //Shaker Sorting
         for (int j = a.length - 1; j > 0; j--) {
             for (int i = 0; i < j; i++) {
                 if (a[i] > a[i + 1]) {
@@ -29,8 +29,14 @@ public class BubbleSortOfArray {
                     a[i] = t;
                 }
             }
+            for (int k = a.length; k > j; k--) {
+                if (a[k] < a[k + 1]) {
+                    int t = a[k + 1];
+                    a[k + 1] = a[k];
+                    a[k] = t;
+                }
+            }
         }
-
         long finish = System.currentTimeMillis();
         System.out.println(Arrays.toString(a));
         int sec = (int)(finish-start);
