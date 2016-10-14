16b74fc8aaf6224e66c2522dd6e2ab22b82416f4
McLoy <tkachenko.vlad@hotmail.com>
2016-10-14 13:29:09 +0300

    update

---
+++ src/com/ua/vtkachenko/easytask15/SimpleSortArray.java
@@ -0,0 +1,36 @@
+package com.ua.vtkachenko.easytask15;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class SimpleSortArray {
+
+    public static void main(String[] args) {
+
+        int[] a = new int[30];
+
+        Random r = new Random();
+
+        for (int i = 0; i < a.length; i++) {
+            a[i] = r.nextInt(100);
+        }
+
+        System.out.println(Arrays.toString(a));
+
+        //Simple Sorting
+        for (int i = 0; i < a.length - 1;) {
+            if (a[i] > a[i + 1]){
+                int t = a[i + 1];
+                a[i + 1] = a[i];
+                a[i] = t;
+                i = 0;
+
+            } else i++;
+            if (i == a.length - 1) break;
+        }
+
+        System.out.println(Arrays.toString(a));
+
+
+    }
+}
