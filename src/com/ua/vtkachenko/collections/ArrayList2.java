b90d7c245c1ffabf58fe2f5edf2b6a681f23706a
McLoy <tkachenko.vlad@hotmail.com>
2016-10-20 01:22:25 +0300

    update

---
+++ src/com/ua/vtkachenko/collections/ArrayList2.java
@@ -0,0 +1,53 @@
+package com.ua.vtkachenko.collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayList2 implements List {
+
+    private int[] elementData;
+    private int size;
+
+    boolean add(){
+        return false;
+    }
+
+    boolean remove(){
+        return false;
+    }
+
+    boolean containsAll(){
+        return false;
+    }
+
+    boolean addAll(){
+        return false;
+    }
+
+    boolean removeAll(){
+        return false;
+    }
+
+    boolean retainAll(){
+        return false;
+    }
+
+    void replaceAll(){}
+
+    void sort(){}
+
+    boolean equals(){
+
+        return false;
+    }
+
+    int hashcode(){
+        return 0;
+    }
+
+     get(int x){
+        return;
+    }
+
+
+}
