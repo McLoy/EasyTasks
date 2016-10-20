86ab99329d5794815db90762ea730be763647a8c
McLoy <tkachenko.vlad@hotmail.com>
2016-10-20 18:05:59 +0300

    update

--- src/com/ua/vtkachenko/collections/ArrayList2.java
+++ src/com/ua/vtkachenko/collections/ArrayList2.java
@@ -1,53 +1,132 @@
 package com.ua.vtkachenko.collections;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class ArrayList2 implements List {
 
-    private int[] elementData;
-    private int size;
+    public int size(){
+    return 0;
+    }
 
-    boolean add(){
+    public boolean isEmpty(){
         return false;
     }
 
-    boolean remove(){
+    public boolean contains(Object o){
         return false;
     }
 
-    boolean containsAll(){
+    public Iterator<E> iterator(){
+        return new Iterator<E>;
+    }
+
+    public Object[] toArray(){
+        return new Object[];
+    }
+
+    public <T> T[] toArray(T[] a){
+        return T[];
+    }
+
+    public boolean add(E e){
         return false;
     }
 
-    boolean addAll(){
+    public boolean remove(Object o){
         return false;
     }
 
-    boolean removeAll(){
+    public boolean containsAll(Collection<?> c){
         return false;
     }
 
-    boolean retainAll(){
+    public boolean addAll(Collection<? extends E> c){
         return false;
     }
 
-    void replaceAll(){}
-
-    void sort(){}
-
-    boolean equals(){
-
+    public boolean addAll(int index, Collection<? extends E> c){
         return false;
     }
 
-    int hashcode(){
+    public boolean removeAll(Collection<?> c){
+        return false;
+    }
+
+    ///
+    boolean retainAll(Collection<?> c){
+        return new UnsupportedOperationException();
+    };
+
+    default void replaceAll(UnaryOperator<E> operator) {
+        Objects.requireNonNull(operator);
+        final ListIterator<E> li = this.listIterator();
+        while (li.hasNext()) {
+            li.set(operator.apply(li.next()));
+        }
+    }
+
+    @SuppressWarnings({"unchecked", "rawtypes"})
+    default void sort(Comparator<? super E> c) {
+         new UnsupportedOperationException();
+//        Object[] a = this.toArray();
+//        Arrays.sort(a, (Comparator) c);
+//        ListIterator<E> i = this.listIterator();
+//        for (Object e : a) {
+//            i.next();
+//            i.set((E) e);
+//        }
+    }
+
+    public void clear(){
+
+    }
+
+    public boolean equals(Object o){
+        return false;
+    }
+
+    public int hashCode(){
         return 0;
     }
 
-     get(int x){
-        return;
+    public E get(int index){
+        return E;
     }
 
+    public  E set(int index, E element){
+        return E;
+    }
+
+    public void add(int index, E element){
+
+    }
+
+    public E remove(int index){
+        return E;
+    }
+
+    public int indexOf(Object o){
+        return 0;
+    }
+
+    public int lastIndexOf(Object o){
+        return 0;
+    }
+
+    ListIterator<E> listIterator(){
+        return new UnsupportedOperationException();
+    }
+
+    ListIterator<E> listIterator(int index){
+       return new UnsupportedOperationException();
+    }
+
+    List<E> subList(int fromIndex, int toIndex){
+        return new UnsupportedOperationException();
+    }
+
+    default Spliterator<E> spliterator() {
+
+        new UnsupportedOperationException();
+        return Spliterators.spliterator(this, Spliterator.ORDERED);
+    }
 
 }
