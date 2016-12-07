b47cfd1e647be1045cf4eecb83497b1d8555cecf
McLoy <tkachenko.vlad@hotmail.com>
2016-12-07 18:02:13 +0200

    concurrency example 2

--- src/ua/com/vtkachenko/threads/example2guessdigit/MyThredy.java
+++ src/ua/com/vtkachenko/threads/example2guessdigit/MyThredy.java
@@ -12,30 +12,32 @@ public class MyThredy extends Thread {
 
     @Override
     public void run() {
-        int findMeM = 2;
-        int findMeL = 8;
+        int findMeM = 7;
+        int findMeL = 7;
         int f;
         Random r = new Random();
-        if (name.equals("M")) {
+        if (name.equals("Рядовой")) {
             f = 0;
             while ((f = r.nextInt(20)) != findMeM) {
-                System.out.println(name + " = " + f + " ?");
-                try {
-                    sleep(1000);
-                } catch (InterruptedException e) {
-                }
+                System.out.println(name + " = может это " + f + " ?");
+//                try {
+//                    sleep(300);
+//                } catch (InterruptedException e) {
+//                    System.out.println("Exception of first thread");
+//                }
             }
             System.out.println(name + " = Bingo!(" + f + ")");
         }
 
-        if (name.equals("L")) {
+        if (name.equals("Лейтенант")) {
             f = 0;
             while ((f = r.nextInt(20)) != findMeL) {
-                System.out.println(name + " = " + f + " ?");
-                try {
-                    sleep(1000);
-                } catch (InterruptedException e) {
-                }
+                System.out.println(name + " = может это " + f + " ?");
+//                try {
+//                    sleep(300);
+//                } catch (InterruptedException e) {
+//                    System.out.println("Exception of second thread");
+//                }
             }
             System.out.println(name + " = Bingo!(" + f + ")");
         }
--- src/ua/com/vtkachenko/threads/example2guessdigit/Program.java
+++ src/ua/com/vtkachenko/threads/example2guessdigit/Program.java
@@ -6,21 +6,23 @@ import static java.lang.Thread.sleep;
 
 public class Program {
     public static void main(String[] args) {
-        MyThredy mt1 = new MyThredy("M");
-        MyThredy mt2 = new MyThredy("L");
+        int findMe1 = 7;
+        System.out.println("Угадываем число " + findMe1);
+        MyThredy mt1 = new MyThredy("Лейтенант");
+        MyThredy mt2 = new MyThredy("Рядовой");
         mt1.start();
         mt2.start();
 
-        String name1 = "S";
-        int findMe1 = 3;
+        String name1 = "Прапор";
         int f1 = 0;
         Random r1 = new Random();
         while ((f1 = r1.nextInt(20)) != findMe1) {
-            System.out.println(name1 + " = " + f1 + " ?");
-            try {
-                sleep(1000);
-            } catch (InterruptedException e) {
-            }
+            System.out.println(name1 + " = может это " + f1 + " ?");
+//            try {
+//                sleep(300);
+//            } catch (InterruptedException e) {
+//                System.out.println("Exception of main flow");
+//            }
         }
         System.out.println(name1 + " = Bingo!(" + f1 + ")");
     }
