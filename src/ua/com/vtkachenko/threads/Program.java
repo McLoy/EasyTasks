35304a7a5278d4cf0054bcbe5b19e747a4a81db9
McLoy <tkachenko.vlad@hotmail.com>
2016-12-07 12:53:46 +0200

    concurrency

--- src/ua/com/vtkachenko/threads/Program.java
+++ src/ua/com/vtkachenko/threads/Program.java
@@ -1,5 +1,7 @@
 package ua.com.vtkachenko.threads;
 
+import static java.lang.Thread.sleep;
+
 public class Program {
     static SomeThing someThing;
 
@@ -7,6 +9,13 @@ public class Program {
         someThing = new SomeThing();
         Thread myThread = new Thread(someThing);
         myThread.start();
+        for (int i = 0; i < 10; i++) {
+            if (! myThread.isAlive()){break;}
+            System.out.println("Основной поток - " + i);
+            try{
+                sleep(1000);
+            }catch(InterruptedException e){}
+        }
         System.out.println("Главный поток завершен...");
     }
 }
--- src/ua/com/vtkachenko/threads/SomeThing.java
+++ src/ua/com/vtkachenko/threads/SomeThing.java
@@ -1,10 +1,17 @@
 package ua.com.vtkachenko.threads;
 
+import static java.lang.Thread.sleep;
+
 public class SomeThing  implements  Runnable{
 
     @Override
     public void run() {
-        System.out.println("Привет из побочного потока!");
+        for (int i = 0; i < 5 ; i++) {
+            System.out.println("Привет из побочного потока! - " + i);
+            try{
+                sleep(1000);
+            }catch(InterruptedException e){}
+        }
     }
 }
 
