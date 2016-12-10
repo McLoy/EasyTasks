3a057d2fa8f1d42d41187380cfa1039bd0f026cb
McLoy <tkachenko.vlad@hotmail.com>
2016-12-10 14:38:33 +0200

    concurrency example

--- src/ua/com/vtkachenko/threads/examplespeed64long/FalseSharing.java
+++ src/ua/com/vtkachenko/threads/examplespeed64long/FalseSharing.java
@@ -7,49 +7,53 @@ import java.util.concurrent.Executors;
 
 public class FalseSharing {
 
-//    volatile static long value0 = 0;
-//    volatile static long value1 = 0;
-//    volatile static long value2 = 0;
-//    volatile static long value3 = 0;
-//    volatile static long value4 = 0;
-//    volatile static long value5 = 0;
-//    volatile static long value6 = 0;
-//    volatile static long value7 = 0;
-//    volatile static long value8 = 0;
-//
-//    public static void main(String[] args) throws InterruptedException{
-//        ExecutorService pool = Executors.newFixedThreadPool(2);
-//        final CountDownLatch latch0 = new CountDownLatch(2);
-//        final CountDownLatch latch1 = new CountDownLatch(1);
-//        final CountDownLatch latch2 = new CountDownLatch(2);
-//        pool.submit(new Callable<Void>() {
-//            public Void call() throws Exception {
-//                latch0.countDown();
-//                latch1.await();
-//                long t0 = System.nanoTime();
-//                for (int k = 0; k < 100; k++) {
-//                    value0 = value0 * k;
-//                }
-//                long t1 = System.nanoTime();
-//                System.out.println((t1 - t0) / 1000000 + "ms");
-//                latch2.countDown();
-//                return null;
-//            }
-//        });
-//        pool.submit(new Callable<Void>() {
-//            public Void call() throws Exception {
-//                latch0.countDown();
-//                latch1.await();
-//                long t0 = System.nanoTime();
-//                for (int k = 0; k < 100; k++) {
-//                    value8 = value8 * k;
-//                }
-//                long t1 = System.nanoTime();
-//                System.out.println((t1 - t0) / 1000000 + "ms");
-//                latch2.countDown();
-//                return null;
-//            }
-//        });
-//
-//    }
+    volatile static long value0 = 0;
+    volatile static long value1 = 0;
+    volatile static long value2 = 0;
+    volatile static long value3 = 0;
+    volatile static long value4 = 0;
+    volatile static long value5 = 0;
+    volatile static long value6 = 0;
+    volatile static long value7 = 0;
+    volatile static long value8 = 0;
+
+    public static void main(String[] args) throws InterruptedException{
+        ExecutorService pool = Executors.newFixedThreadPool(2);
+        final CountDownLatch latch0 = new CountDownLatch(2);
+        final CountDownLatch latch2 = new CountDownLatch(2);
+        pool.submit(new Callable<Void>() {
+            public Void call() throws Exception {
+                latch0.countDown();
+                latch0.await();
+                long t0 = System.nanoTime();
+                for (int k = 0; k < 100_000_000; k++) {
+                    value0 = value0 * k;
+                }
+                long t1 = System.nanoTime();
+                System.out.println((t1 - t0) / 1000000 + "ms");
+                latch2.countDown();
+                return null;
+            }
+        });
+        pool.submit(new Callable<Void>() {
+            public Void call() throws Exception {
+                latch0.countDown();
+                latch0.await();
+                long t0 = System.nanoTime();
+                for (int k = 0; k < 100_000_000; k++) {
+                    value8 = value8 * k;
+                }
+                long t1 = System.nanoTime();
+                System.out.println((t1 - t0) / 1000000 + "ms");
+                latch2.countDown();
+                return null;
+            }
+        });
+
+//        latch0.wait();
+//        latch1.countDown();
+        latch2.await();
+
+        pool.shutdownNow();
+    }
 }
